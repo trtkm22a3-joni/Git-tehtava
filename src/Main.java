eb1e976acd0ec0979e37d6f5a6337b7ffa4e9b26
trtkm22a3-joni <joni22007@student.hamk.fi>
2022-12-12 01:10:54 +0200

    uusi luokka

--- src/Main.java
+++ src/Main.java
@@ -2,51 +2,60 @@
 public class Main {
 
 	public static void main(String[] args) {
-		
+
 		Auto auto1 = new Auto();
 		auto1.merkki = "Lincoln";
 		auto1.malli = "Town Car";
 		auto1.bensanMaara = 60;
 		auto1.naytaTiedot();
 		auto1.kiihdyta();
+		auto1.jarruta();
 		auto1.naytaTiedot();
-
-
+		auto1.tankkaa(10);
 
 	}
 
 } // Main luokan lopetus
 
 class Auto {
-	
+
+	//Määritellään attribuutit
 	public String merkki;
 	public String malli;
 	public int bensanMaara;
-	
-	public void jarruta() {
-		
-		System.out.println("Auto jarruttaa");
-		
-	}
-	
-	public void kiihdyta() {
-		
-		System.out.println("Auto kiihtyy");
-		bensanMaara--;
-		
-	}
-	public void naytaTiedot() {
-		
-		System.out.println("Merkki: " + merkki + "\n" + "Malli: " + malli + "\n" + "Polttoaineen määrä (bensa): " + bensanMaara + " litraa" + "\n");
-		
-		
-		
-	}
-	
-	
 
+	//Määritellään metodit
+	public void jarruta() {
+
+		System.out.println("Auto jarruttaa\n");
+
+	}
+
+	public void kiihdyta() {
+
+		if (bensanMaara <= 0) {
+
+			System.out.println("Auto ei enää liiku. Polttoaineen määrä täytyy olla suurempi kuin 0 litraa");
+
+		} else {
+
+			System.out.println("Auto kiihtyy\n");
+			bensanMaara--;
+		}
+	}
+
+	public void naytaTiedot() {
+
+		System.out.println("Merkki: " + merkki + "\n" + "Malli: " + malli + "\n" + "Polttoaineen määrä (bensa): " + bensanMaara + " litraa" + "\n");
+
+	}
 	
-	
-	
-	
+	public void tankkaa(int tankkaus) {
+		
+		
+		System.out.println("Tankissa polttoainetta: " + bensanMaara + " litraa");
+		System.out.println("Tankkaus: " + tankkaus + " litraa");
+		System.out.println("Tankissa polttoainetta tankkauksen jälkeen: " + (bensanMaara + tankkaus) + " litraa");
+	}
+
 }
