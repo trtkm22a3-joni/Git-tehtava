df2123d47a6113bcc9d94c15eb19363f58370148
trtkm22a3-joni <joni22007@student.hamk.fi>
2022-12-12 00:59:25 +0200

    -

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,13 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		System.out.println("testi");
+		Auto auto1 = new Auto();
+		auto1.merkki = "Lincoln";
+		auto1.malli = "Town Car";
+		auto1.bensanMaara = 60;
+		auto1.naytaTiedot();
+		auto1.kiihdyta();
+		auto1.naytaTiedot();
 
 
 
@@ -15,7 +21,30 @@ class Auto {
 	
 	public String merkki;
 	public String malli;
-	public double bensanMaara;
+	public int bensanMaara;
+	
+	public void jarruta() {
+		
+		System.out.println("Auto jarruttaa");
+		
+	}
+	
+	public void kiihdyta() {
+		
+		System.out.println("Auto kiihtyy");
+		bensanMaara--;
+		
+	}
+	public void naytaTiedot() {
+		
+		System.out.println("Merkki: " + merkki + "\n" + "Malli: " + malli + "\n" + "Polttoaineen määrä (bensa): " + bensanMaara + " litraa" + "\n");
+		
+		
+		
+	}
+	
+	
+
 	
 	
 	
